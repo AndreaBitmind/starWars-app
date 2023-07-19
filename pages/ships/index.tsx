0d221295e9faf7aeb58b664ca1071b8f5f260fa3
Andrea Martinez <andrea.maort@gmail.com>
2023-07-19 17:26:14 +0200

    feat: added a list of ships

---
+++ pages/ships/index.tsx
@@ -0,0 +1,35 @@
+import styles from "../../styles/page.module.css";
+import Layout from "../../src/app/layout";
+
+interface ShipMainProps {
+  id: number;
+  name: string;
+}
+
+type ShipMainPropsArray = ShipMainProps[];
+
+const ships: ShipMainPropsArray = [
+  { id: 1, name: "ship1" },
+  { id: 2, name: "ship2" },
+  { id: 3, name: "ship3" },
+  { id: 4, name: "ship4" },
+  { id: 5, name: "ship5" },
+];
+
+export default function Home({ id, name }: ShipMainProps): JSX.Element {
+  return (
+    <main className={styles.main}>
+      <h1>SHIPS</h1>
+      <section className={styles.section}>
+        <ul className={styles.grid}>
+          {ships.map((ship: ShipMainProps) => (
+            <li className={styles.li} key={ship.id}>
+              <div>{ship.name}</div>
+              <span>{ship.id}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
