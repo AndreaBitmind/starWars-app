0d3cfe810859ac59c78c0988854cc5c6c1c45894
Andrea Martinez <andrea.maort@gmail.com>
2023-07-20 12:10:25 +0200

    feat: added resources to sidebar

--- componentes/Sidebar.tsx
+++ componentes/Sidebar.tsx
@@ -1,14 +1,27 @@
 import Link from "next/link";
 import styles from "../styles/sidebar.module.css";
 
-const Sidebar = () => {
+const resources = [
+  { name: "films", path: "/films" },
+  { name: "people", path: "/people" },
+  { name: "planets", path: "/planets" },
+  { name: "species", path: "/species" },
+  { name: "starships", path: "/starships" },
+  { name: "vehicles", path: "/vehicles" },
+];
+
+const Sidebar: React.FC = () => {
   return (
     <aside className={styles.sidebar}>
       <nav>
         <ul className={styles.menu}>
-          <li>
-            <Link href="/ships">Ships</Link>
-          </li>
+          {resources.map((resource) => (
+            <li key={resource.name}>
+              <Link href={resource.path}>
+                <div>{resource.name}</div>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
